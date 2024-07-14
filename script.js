5ad6634198f00049cb26ec6b17006098a4d9f799
zwhahn <117476820+zwhahn@users.noreply.github.com>
2024-07-14 12:54:10 -0400

    Add reset button

--- script.js
+++ script.js
@@ -45,24 +45,38 @@ function playRound(playerSelection, computerSelection) {
     return result
     }
 
-let playerCount = 0;
-let computerCount = 0;
-let round = 1;
-let maxRound = 5;
+var playerCount = 0;
+var computerCount = 0;
+let round = 0;
+const MAX_ROUND = 5;
 
-function playGame(playerCount, computerCount, playerSelection, currentRound, maxRound) {
+function playGame(playerSelection, round, MAX_ROUND) {
     let computerSelection = getComputerChoice()
     let winner = checkWinner(playerSelection, computerSelection)
     if (winner == "Player") {
-        playerCount = playerCount++}  
+        playerCount++}  
     if (winner == "Computer") {
-        computerCount = computerCount++}
+        computerCount++}
+    roundWinner.textContent = `Round ${round} Winner: ${winner}`
     playerScore.textContent = `Player: ${playerCount}`;
     cpuScore.textContent = `Computer: ${computerCount}`;
-    if (currentRound === maxRound){
-        round = 1;
-        result.textContent = `Winner: ${winner}`;
-    }
+    if (round === MAX_ROUND){
+        round = 0;
+        reset.appendChild(resetButton);
+        if (playerCount > computerCount){
+            result.textContent = `Winner: Player`;
+        }
+        else if (computerCount > playerCount){
+            result.textContent = `Winner: CPU`;
+        }
+        else if (playerCount === computerCount){
+            result.textContent = `Tie!`;
+        }
+        else {
+            result.textContent = `ERROR: No result`;
+        }
+        }
+    return round
     }
 
 
@@ -70,19 +84,22 @@ const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         playerSelection = button.id;
-        currentRound = round++
-        playGame(playerCount, computerCount, playerSelection, currentRound, maxRound);
+        if (round === 0){
+            playerCount = 0;
+            computerCount = 0;
+        }
+        round++
+        round = playGame(playerSelection, round, MAX_ROUND);
     })
 })
 
 const result = document.querySelector(".result");
 const playerScore = document.querySelector(".player-score");
 const cpuScore = document.querySelector(".cpu-score");
+const roundWinner = document.querySelector(".round-winner");
 
-
-// click button
-// winner is decided
-// winner +1
-// round +1
-// After 5 rounds,  winner has highest scrore
-// round is reset to 0
+const reset = document.querySelector(".reset");
+const resetButton = document.createElement("button");
+resetButton.textContent = `Play Again`;
+resetButton.style.height = `60px`;
+resetButton.style.width = `140px`;
